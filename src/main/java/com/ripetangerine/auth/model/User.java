61cb5d85e1d28a0775fc44ad5d6d1044fb4a9eb7
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-15 11:45:36 +0900

    style: User entity

--- src/main/java/com/ripetangerine/auth/model/User.java
+++ src/main/java/com/ripetangerine/auth/model/User.java
@@ -1,13 +1,16 @@
 package com.ripetangerine.auth.model;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class User {
-    Long id;
-    String username;
-    String password;
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
 }
